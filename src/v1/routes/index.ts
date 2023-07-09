ee2818668d7282b1353ea3342c4b1d8f71d7af7c
Gabriel <gkime91@gmail.com>
2023-07-08 18:23:18 -0700

    removed test route index.

--- src/v1/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const v1Router = express.Router();
-
-v1Router.route('/').get((req, res) => {
-  res.send(`<h2>Hello from ${req.baseUrl} </h2>`);
-});
-
-export default v1Router;
