d9bd9b2f02fab18ae633390d652c6772e8a94c1e
Gabriel <gkime91@gmail.com>
2023-07-08 19:17:30 -0700

    connected controllers to services.

--- src/v1/controllers/recipeController.ts
+++ src/v1/controllers/recipeController.ts
@@ -1,10 +1,13 @@
 import {Request, Response, NextFunction} from 'express';
+import { getAllRecipesService, getRecipeByIdService } from '../services/recipeService'
 
 export const getAllRecipes = (req: Request, res: Response) => {
-  res.send("Get all recipes")
+  const allRecipes = getAllRecipesService();
+  res.send("Get all recipes");
 };
 
 export const getRecipeById = (req: Request, res: Response) => {
+  const recipeById = getRecipeByIdService();
   res.send("get a recipe by id");
 };
 
